10d6ab78b191db8844a980852074f8c86e097224
thibault <thibault.dereu@gmail.com>
2021-01-26 11:41:03 +0100

    add h2 for unit testing and change jersey config to make it work with fat jar

--- myvotingsystem-backend/src/main/java/fr/thibaultd/myvotingsystem/backend/configuration/JerseyConfig.java
+++ myvotingsystem-backend/src/main/java/fr/thibaultd/myvotingsystem/backend/configuration/JerseyConfig.java
@@ -2,11 +2,16 @@ package fr.thibaultd.myvotingsystem.backend.configuration;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
+import fr.thibaultd.myvotingsystem.backend.api.ElectionResource;
+import fr.thibaultd.myvotingsystem.backend.api.VoteResource;
 
 @Component
 public class JerseyConfig extends ResourceConfig {
 
   public JerseyConfig() {
-    packages("fr.thibaultd.myvotingsystem.backend");
+    // packages("fr.thibaultd.myvotingsystem.backend"); // doesn't work with fat jar
+    register(ElectionResource.class);
+    register(VoteResource.class);
+
   }
 }
